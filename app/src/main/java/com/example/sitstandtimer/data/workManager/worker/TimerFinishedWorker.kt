72f327831f3d82a97e8634395f6d491c0f35541a
Charlie Weidner <109787089+Charlesberg22@users.noreply.github.com>
2024-10-25 16:32:38 +1030

    Fixed an error with getForegroundInfo

--- app/src/main/java/com/example/sitstandtimer/data/workManager/worker/TimerFinishedWorker.kt
+++ app/src/main/java/com/example/sitstandtimer/data/workManager/worker/TimerFinishedWorker.kt
@@ -19,14 +19,14 @@ class TimerFinishedWorker(
     private val vibrationHelper: VibrationHelper
 ) : CoroutineWorker(context, workerParameters) {
 
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        val type = inputData.getString(typeKey)
+        val notification = timerNotificationHelper.timerFinishedBuilder(type)
+        return ForegroundInfo(TIMER_FINISHED_NOTIFICATION_ID, notification)
+    }
+
     override suspend fun doWork(): Result {
         return try {
-
-            val timerType = inputData.getString(typeKey)
-
-            val notification = timerNotificationHelper.timerFinishedBuilder(timerType)
-
-            setForeground(ForegroundInfo(TIMER_FINISHED_NOTIFICATION_ID, notification))
 
             if (inputData.getBoolean(silentKey, false)) {
                 vibrationHelper.prepare()
